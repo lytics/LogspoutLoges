89eef13e6f60f83874e7746e1b5a5a484a0ee5e8
Matt Braymer-Hayes <matthew.braymer-hayes@lytics.com>
2019-01-19 23:21:58 -0800

    Add extra logging

--- go.mod
+++ go.mod
@@ -9,7 +9,6 @@ require (
 	github.com/gopherjs/gopherjs v0.0.0-20181103185306-d547d1d9531e // indirect
 	github.com/jtolds/gls v4.2.1+incompatible // indirect
 	github.com/kr/pretty v0.1.0 // indirect
-	github.com/looplab/logspout-logstash v0.0.0-20171130125839-68a4e47e757d
 	github.com/mattbaird/elastigo v0.0.0-20170123220020-2fe47fd29e4b
 	github.com/sirupsen/logrus v1.3.0
 	github.com/smartystreets/assertions v0.0.0-20180927180507-b2de0cb4f26d // indirect
--- logspoutloges.go
+++ logspoutloges.go
@@ -15,6 +15,7 @@ var elastigoConn *elastigo.Conn
 
 func init() {
 	router.AdapterFactories.Register(NewLogesAdapter, "logspoutloges")
+	log.SetLevel(log.DebugLevel)
 }
 
 // LogesAdapter is an adapter that streams TCP JSON to Elasticsearch
@@ -26,62 +27,96 @@ type LogesAdapter struct {
 
 // NewLogesAdapter creates a LogesAdapter with TCP Elastigo BulkIndexer as the default transport.
 func NewLogesAdapter(route *router.Route) (router.LogAdapter, error) {
-
-	addr := route.Address
+	log.Debugf("new LogesAdapter for route: %+v", route)
 
 	elastigoConn = elastigo.NewConn()
 	// The old standard for host was including :9200
-	esHost := strings.Replace(addr, ":9200", "", -1)
-	log.Infof("esHost variable: %s\n", esHost)
+	host := strings.Replace(route.Address, ":9200", "", -1)
+	hosts := strings.Split(host, "+")
+	log.Debugf("esHost variable: %s", hosts)
 
-	elastigoConn.SetHosts([]string{esHost})
+	elastigoConn.SetHosts(hosts)
 	indexer := elastigoConn.NewBulkIndexerErrors(50, 120)
 	indexer.Sender = func(buf *bytes.Buffer) error {
-		log.Infof("es writing: %d bytes", buf.Len())
-		return indexer.Send(buf)
+		l := buf.Len()
+		err := indexer.Send(buf)
+		if err != nil {
+			log.Warnf("failed to send to Elasticsearch: %v", err)
+			return err
+		}
+		log.Debugf("sent %d of %d bytes to Elasticsearch", l-buf.Len(), l)
+		return nil
 	}
+	// TODO: How/when is the indexer closed?
 	indexer.Start()
 
-	return &LogesAdapter{
+	l := &LogesAdapter{
 		route:   route,
 		conn:    elastigoConn,
 		indexer: indexer,
-	}, nil
+	}
+	log.Debugf("created adapter: %+v", l)
+	return l, nil
 }
 
 // Stream implements the router.LogAdapter interface.
 func (a *LogesAdapter) Stream(logstream chan *router.Message) {
+	log.Debugf("started streaming for adapter: %+v", a)
 	lid := 0
 	for m := range logstream {
+		log.Debugf("new message: %+v", m)
 		lid++
-		// Un-escape the newline characters so logs look nice
-		m.Data = EncodeNewlines(m.Data)
+
+		var mess string
+		var fields map[string]interface{}
+		d := []byte(m.Data)
+
+		if json.Valid(d) {
+			var msgField message
+			if err := json.Unmarshal(d, &msgField); err != nil {
+				log.Warnf("failed to unmarshal data %v: %v", m.Data, err)
+			}
+			mess = msgField.Message
+
+			var fields map[string]interface{}
+			if err := json.Unmarshal(d, &fields); err != nil {
+				log.Warnf("failed to unmarshal data %v: %v", m.Data, err)
+			} else {
+				delete(fields, "message")
+			}
+		} else {
+			mess = m.Data
+		}
 
 		msg := LogesMessage{
 			Source:    m.Container.Config.Hostname,
 			Type:      "logs",
 			Timestamp: time.Now(),
-			Message:   m.Data,
+			Message:   mess,
+			Fields:    fields,
 			Name:      m.Container.Name,
 			ID:        m.Container.ID,
 			Image:     m.Container.Config.Image,
 			Hostname:  m.Container.Config.Hostname,
 			LID:       lid,
 		}
+
 		js, err := json.Marshal(msg)
 		if err != nil {
-			log.Println("loges:", err)
+			log.Warnf("failed to marshal message: %v", err)
 			continue
 		}
 
 		idx := "logstash-" + m.Time.Format("2006.01.02")
 		if err := a.indexer.Index(idx, "logs", "", "", "30d", &m.Time, js); err != nil {
-			log.Errorf("Index(ing) error: %v\n", err)
+			log.Warnf("failed to index message: %v", err)
 		}
+		log.Debugf("indexed message: %+v", msg)
 	}
+	log.Debugf("done streaming for adapter: %+v", a)
 }
 
-// LogesMessage Encapsulates the log data for Elasticsearch
+// LogesMessage encapsulates the log data for Elasticsearch
 type LogesMessage struct {
 	Source      string                 `json:"@source"`
 	Type        string                 `json:"@type"`
@@ -90,9 +125,13 @@ type LogesMessage struct {
 	Tags        []string               `json:"@tags,omitempty"`
 	IndexFields map[string]interface{} `json:"@idx,omitempty"`
 	Fields      map[string]interface{} `json:"@fields"`
-	Name        string                 `json:"docker.name"`
-	ID          string                 `json:"docker.id"`
-	Image       string                 `json:"docker.image"`
-	Hostname    string                 `json:"docker.hostname"`
-	LID         int                    `json:"logspoutloges.id"`
+	Name        string                 `json:"docker_name"`
+	ID          string                 `json:"docker_id"`
+	Image       string                 `json:"docker_image"`
+	Hostname    string                 `json:"docker_hostname"`
+	LID         int                    `json:"logspoutloges_id"`
+}
+
+type message struct {
+	Message string `json:"message"`
 }
