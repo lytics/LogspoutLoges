dea6f9f3b591016e5f7a1220764c32da3d04119c
Matt Braymer-Hayes <matthew.braymer-hayes@lytics.com>
2019-01-31 12:25:42 -0800

    Replace elastigo with elastic.

--- go.mod
+++ go.mod
@@ -1,16 +1,12 @@
 module github.com/lytics/logspoutloges
 
 require (
-	github.com/araddon/gou v0.0.0-20190110011759-c797efecbb61 // indirect
-	github.com/bitly/go-hostpool v0.0.0-20171023180738-a3a6125de932 // indirect
 	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869
+	github.com/fortytw2/leaktest v1.3.0 // indirect
 	github.com/fsouza/go-dockerclient v1.3.3 // indirect
 	github.com/gliderlabs/logspout v3.2.6+incompatible
-	github.com/gopherjs/gopherjs v0.0.0-20181103185306-d547d1d9531e // indirect
-	github.com/jtolds/gls v4.2.1+incompatible // indirect
 	github.com/kr/pretty v0.1.0 // indirect
-	github.com/mattbaird/elastigo v0.0.0-20170123220020-2fe47fd29e4b
 	github.com/sirupsen/logrus v1.3.0
-	github.com/smartystreets/assertions v0.0.0-20180927180507-b2de0cb4f26d // indirect
-	github.com/smartystreets/goconvey v0.0.0-20181108003508-044398e4856c // indirect
+	golang.org/x/sync v0.0.0-20181221193216-37e7f081c4d4 // indirect
+	gopkg.in/olivere/elastic.v3 v3.0.75
 )
--- logspoutloges.go
+++ logspoutloges.go
@@ -1,59 +1,68 @@
 package logspoutloges
 
 import (
-	"bytes"
 	"encoding/json"
+	"fmt"
+	"net/url"
+	"os"
 	"strings"
 	"time"
 
 	"github.com/gliderlabs/logspout/router"
-	elastigo "github.com/mattbaird/elastigo/lib"
 	log "github.com/sirupsen/logrus"
+	elastic "gopkg.in/olivere/elastic.v3"
 )
-
-var elastigoConn *elastigo.Conn
 
 func init() {
 	router.AdapterFactories.Register(NewLogesAdapter, "logspoutloges")
-	log.SetLevel(log.DebugLevel)
+	if os.Getenv("DEBUG") != "" {
+		log.Infof("Debug logging enabled.")
+		log.SetLevel(log.DebugLevel)
+	}
 }
 
 // LogesAdapter is an adapter that streams TCP JSON to Elasticsearch
 type LogesAdapter struct {
-	conn    *elastigo.Conn
-	route   *router.Route
-	indexer *elastigo.BulkIndexer
+	route *router.Route
+	bp    *elastic.BulkProcessor
 }
 
-// NewLogesAdapter creates a LogesAdapter with TCP Elastigo BulkIndexer as the default transport.
+// NewLogesAdapter creates a LogesAdapter with Elasticsearch as the default transport.
 func NewLogesAdapter(route *router.Route) (router.LogAdapter, error) {
 	log.Debugf("new LogesAdapter for route: %+v", route)
 
-	elastigoConn = elastigo.NewConn()
-	// The old standard for host was including :9200
-	host := strings.Replace(route.Address, ":9200", "", -1)
-	hosts := strings.Split(host, "+")
-	log.Debugf("esHost variable: %s", hosts)
+	hosts := strings.Split(route.Address, "+")
+	hosts = normalize(hosts)
 
-	elastigoConn.SetHosts(hosts)
-	indexer := elastigoConn.NewBulkIndexerErrors(50, 120)
-	indexer.Sender = func(buf *bytes.Buffer) error {
-		l := buf.Len()
-		err := indexer.Send(buf)
-		if err != nil {
-			log.Warnf("failed to send to Elasticsearch: %v", err)
-			return err
-		}
-		log.Debugf("sent %d of %d bytes to Elasticsearch", l-buf.Len(), l)
-		return nil
+	retrier := elastic.NewBackoffRetrier(elastic.NewExponentialBackoff(time.Millisecond, 5*time.Minute))
+
+	// FIXME: Client is never stopped.
+	c, err := elastic.NewClient(
+		elastic.SetURL(hosts...),
+		elastic.SetErrorLog(log.StandardLogger()),
+		elastic.SetInfoLog(log.StandardLogger()),
+		elastic.SetRetrier(retrier),
+	)
+	if err != nil {
+		log.Warnf("failed to create Elasticsearch client: %v", err)
+		return nil, err
 	}
-	// TODO: How/when is the indexer closed?
-	indexer.Start()
+
+	bp, err := c.BulkProcessor().FlushInterval(time.Minute).Do()
+	if err != nil {
+		log.Warnf("failed to create bulk processor: %v", err)
+		return nil, err
+	}
+
+	// FIXME: Processor is never stopped.
+	if err := bp.Start(); err != nil {
+		log.Warnf("failed to start bulk processor: %v", err)
+		return nil, err
+	}
 
 	l := &LogesAdapter{
-		route:   route,
-		conn:    elastigoConn,
-		indexer: indexer,
+		route: route,
+		bp:    bp,
 	}
 	log.Debugf("created adapter: %+v", l)
 	return l, nil
@@ -62,76 +71,79 @@ func NewLogesAdapter(route *router.Route) (router.LogAdapter, error) {
 // Stream implements the router.LogAdapter interface.
 func (a *LogesAdapter) Stream(logstream chan *router.Message) {
 	log.Debugf("started streaming for adapter: %+v", a)
-	lid := 0
 	for m := range logstream {
-		log.Debugf("new message: %+v", m)
-		lid++
+		var msg string
+		var fields Fields
 
-		var mess string
-		var fields map[string]interface{}
-		d := []byte(m.Data)
-
-		if json.Valid(d) {
-			var msgField message
-			if err := json.Unmarshal(d, &msgField); err != nil {
-				log.Warnf("failed to unmarshal data %v: %v", m.Data, err)
-			}
-			mess = msgField.Message
-
-			var fields map[string]interface{}
-			if err := json.Unmarshal(d, &fields); err != nil {
-				log.Warnf("failed to unmarshal data %v: %v", m.Data, err)
-			} else {
-				delete(fields, "message")
-			}
+		if err := json.Unmarshal([]byte(m.Data), &fields); err == nil && fields.Message != "" {
+			msg = fields.Message
+			fields.Message = ""
 		} else {
-			mess = m.Data
+			// Un-escape the newline characters so logs look nice
+			msg = EncodeNewlines(m.Data)
 		}
+		fields.Host = m.Container.Config.Hostname
+		fields.Image = m.Container.Config.Image
 
-		msg := LogesMessage{
+		l := Log{
 			Source:    m.Container.Config.Hostname,
-			Type:      "logs",
-			Timestamp: time.Now(),
-			Message:   mess,
+			Type:      "logspout",
 			Fields:    fields,
-			Name:      m.Container.Name,
-			ID:        m.Container.ID,
-			Image:     m.Container.Config.Image,
-			Hostname:  m.Container.Config.Hostname,
-			LID:       lid,
-		}
-
-		js, err := json.Marshal(msg)
-		if err != nil {
-			log.Warnf("failed to marshal message: %v", err)
-			continue
+			Timestamp: time.Now(),
+			Message:   msg,
 		}
 
 		idx := "logstash-" + m.Time.Format("2006.01.02")
-		if err := a.indexer.Index(idx, "logs", "", "", "30d", &m.Time, js); err != nil {
-			log.Warnf("failed to index message: %v", err)
-		}
-		log.Debugf("indexed message: %+v", msg)
+		r := elastic.NewBulkIndexRequest().
+			Doc(l).
+			Index(idx).
+			Type("logs")
+
+		a.bp.Add(r)
+		log.Debugf("indexed log: %+v", l)
 	}
 	log.Debugf("done streaming for adapter: %+v", a)
 }
 
-// LogesMessage encapsulates the log data for Elasticsearch
-type LogesMessage struct {
+// Log encapsulates log data for Elasticsearch.
+type Log struct {
 	Source      string                 `json:"@source"`
 	Type        string                 `json:"@type"`
 	Timestamp   time.Time              `json:"@timestamp"`
 	Message     string                 `json:"@message"`
 	Tags        []string               `json:"@tags,omitempty"`
 	IndexFields map[string]interface{} `json:"@idx,omitempty"`
-	Fields      map[string]interface{} `json:"@fields"`
-	Name        string                 `json:"docker_name"`
-	ID          string                 `json:"docker_id"`
-	Image       string                 `json:"docker_image"`
-	Hostname    string                 `json:"docker_hostname"`
-	LID         int                    `json:"logspoutloges_id"`
+	Fields      Fields                 `json:"@fields"`
 }
 
-type message struct {
-	Message string `json:"message"`
+// Fields encapsulates standardized log fields.
+type Fields struct {
+	Host     string `json:"host"`
+	Image    string `json:"image"`
+	Level    string `json:"level,omitempty"`
+	Severity string `json:"severity,omitempty"`
+	Message  string `json:"message,omitempty"`
+	RawTime  string `json:"time,omitempty"`
+	File     string `json:"file,omitempty"`
+	Line     int    `json:"line,omitempty"`
+}
+
+func normalize(urls []string) []string {
+	log.Debugf("normalizing urls: %v", urls)
+	out := make([]string, 0, len(urls))
+	for _, s := range urls {
+		url, err := url.Parse(fmt.Sprintf("http://%v", s)) // Need to add scheme for proper parsing
+		if err != nil {
+			log.Warnf("failed to parse %q to URL: %v", s, err)
+			continue
+		}
+		if url.Port() == "" {
+			log.Warnf("URL %q missing port, fixing for now.", url)
+			url.Host = fmt.Sprintf("%v:%v", url.Hostname(), "9200")
+		}
+		log.Debugf("normalized URL: %v", url)
+		out = append(out, url.String())
+	}
+	log.Debugf("normalized urls: %v", out)
+	return out
 }
